85200c7d166772a6e6d3682788b29ef42c1a42f0
jackbinysh <jack.binysh@gmail.com>
2019-10-30 18:46:17 +0000

    forgot to add this function

--- nematic.h
+++ nematic.h
@@ -4,6 +4,7 @@ extern int n,LL;
 extern double *nx,*ny,*nz;
 extern double *hx,*hy,*hz;
 extern double K1mK2,K3mK2;
+extern double K1,K3;
 extern double q0;
 extern std::string RunName;
 
@@ -13,6 +14,7 @@ void writeStatistics(FILE* fileStats);  // output measurements
 void CalculateHopfionDimensionsXAxis(double* radius,  double* leftedge, double* rightedge, double* height);
 
 
+inline void cross(double ax,double ay,double az,double bx,double by,double bz,double &rx,double &ry,double &rz);
 int i_vr(int l);
 int j_vr(int l);
 int k_vr(int l);
